f5469649c38696c1928194d7050c303996622dd8
AG <415204643@qq.com>
2021-05-19 22:20:27 +0800

    type页面传值

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,35 +4,60 @@ Page({
     //轮播图数组
     swiperList: [],
     //导航数组
-    navigateList: [],
+    navigateList: [
+      {
+        name: "电子产品",
+        image_src:
+          "https://prl.yuyisoft.net/images/%E7%94%B5%E5%AD%90%E4%BA%A7%E5%93%81.png",
+        url: "/pages/confirm/index?type='电子产品'",
+      },
+      {
+        name: "大件家具",
+        image_src:
+          "https://prl.yuyisoft.net/images/%E5%A4%A7%E4%BB%B6%E5%AE%B6%E5%85%B7.png",
+        url: "/pages/confirm/index?type='大件家具'",
+      },
+      {
+        name: "书籍杂志",
+        image_src:
+          "https://prl.yuyisoft.net/images/%E4%B9%A6%E7%B1%8D%E6%9D%82%E5%BF%97.png",
+        url: "/pages/confirm/index?type='书籍杂志'",
+      },
+      {
+        name: "分类科普",
+        image_src:
+          "https://prl.yuyisoft.net/images/%E5%88%86%E7%B1%BB%E7%A7%91%E6%99%AE.png",
+        url: "../science/index",
+      },
+    ],
     navigate2List: [
       {
         name: "可回收物",
         image_src:
           "https://prl.yuyisoft.net/images/%E5%8F%AF%E5%9B%9E%E6%94%B6%E7%89%A9.png",
         url: "../recycle_classified/index",
-        key: 'recycle'
+        key: "recycle",
       },
       {
         name: "有害垃圾",
         image_src:
           "https://prl.yuyisoft.net/images/%E6%9C%89%E5%AE%B3%E5%9E%83%E5%9C%BE.png",
         url: "../harmful_classified/index",
-        key: 'harmful'
+        key: "harmful",
       },
       {
         name: "厨余垃圾",
         image_src:
           "https://prl.yuyisoft.net/images/%E5%8E%A8%E4%BD%99%E5%9E%83%E5%9C%BE.png",
         url: "../kitchen_classified/index",
-        key: 'kitchen'
+        key: "kitchen",
       },
       {
         name: "其他垃圾",
         image_src:
           "https://prl.yuyisoft.net/images/%E5%85%B6%E4%BB%96%E5%9E%83%E5%9C%BE.png",
         url: "../other_classified/index",
-        key: 'other'
+        key: "other",
       },
     ],
   },
